d452e2bbac3fd9c7bbb14d543fb0fe7e77af5bcb
Helen Lindsay <helen.lindsay@chuv.ch>
2022-11-10 09:52:33 +0100

    updates to documentation

---
+++ R/Mimitou_2021.R
@@ -0,0 +1,12 @@
+#' Obtain the Mimitou_2021 data
+#'
+#' Obtain the CITE-seq data from Mimitou et al. (2021).
+#'
+#' @details
+#'Introduces a new method ASAP-seq (ATAC-seq with selected antigen profiling).
+#'@references
+#'
+#'@export
+Mimitou_2021 <- function(){
+
+}
