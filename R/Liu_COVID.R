1bed235e27c41c44630fba9d3148dd043bd9361f
Helen Lindsay <helen.lindsay@chuv.ch>
2023-01-08 19:27:36 +0100

    additions to Liu

--- R/Liu_COVID.R
+++ R/Liu_COVID.R
@@ -1,25 +1,31 @@
 #' Obtain the Liu_COVID data
 #'
-#' Obtain the COVID-19 CITE-seq data of enriched PBMCs from  Liu et al. (2021).
+#' Obtain the COVID-19 CITE-seq data of enriched PBMCs from  Liu \emph{et al.}
+#' (2021).
 #'
 #'@details
-#'CITE-seq with TCR/BCR sequencing of longitudinal samples of PBMCs with FACS
-#'enrichment from 31 hospitalized COVID-19 patients and 14 age- and
+#'CITE-seq with TCR/BCR VDJ sequencing of longitudinal samples of PBMCs
+#'from 31 hospitalized COVID-19 patients and 14 age- and
 #'gender-matched healthy controls.  Up to
 #'four samples from different time points were taken from each COVID-19 patient,
 #'starting from the time of hospital admission.
 #'
+#'Sequencing was performed in 3 batches.
 #'
 #'Data were downloaded from
 #'\url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE161918}
 #'
-#'A code repository is available (see below).
+#'Liu \emph{et al.} also measured serum protein levels and blood cell counts.
+#'These are included in the code repository (see below).
 #'
 #'@references
 #'Liu, Can, et al. "Time-resolved systems immunology reveals a late juncture
 #'linked to fatal COVID-19." Cell 184.7 (2021): 1836-1857.
 #'\doi{10.1016/j.cell.2021.02.018}
-#'@seealso \url{https://github.com/niaid/covid19-time-resolved}
+#'@seealso
+#'Code repository: \url{https://github.com/niaid/covid19-time-resolved}
+#'Data browser:
+#'\url{https:// cellxgene.cziscience.com/collections/ed9185e3-5b82-40c7-9824-b2141590c7f0}
 #'@export
 Liu_COVID <- function(){
 
--- inst/scripts/make_metadata/Liu_COVID.R
+++ inst/scripts/make_metadata/Liu_COVID.R
@@ -1,1 +1,10 @@
 # Sequencing was performed in 3 batches, the files on GEO are separated by batch
+
+# Patient metadata is in individual metadata tables
+
+library(tidyverse)
+
+patient_dat <- do.call(bind_rows,
+                       lapply(liu, function(x) as_tibble(metadata(x))))
+
+patient_dat <-
