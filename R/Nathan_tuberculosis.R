7db7a706e8f42027bd21c9ef48965c37897282bc
helen <helen@hos67669.intranet.chuv>
2023-01-31 15:49:42 +0100

    doc for Nathan

---
+++ R/Nathan_tuberculosis.R
@@ -0,0 +1,32 @@
+#' Obtain the Nathan_tuberculosis data
+#'
+#' Obtain the memory T-cell tuberculosis CITE-seq data from Nathan
+#' \emph{et al. 2021}
+#'
+#'@details
+#'CITE-seq of memory T-cells in 259 individuals, including 128 cases at least
+#'4 years post recovery from tuberculosis (TB) and 131 controls
+#'(household contacts with positive tuberculosis skin test who didn't develop
+#'TB disease). 12 samples have technical replicates.
+#'Detailed genotype and phenotype data is also available from dbGaP on request.
+#'
+#'Data were downloaded from
+#'\url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE158769}
+#'
+#'A code repository is available (see below).
+#'
+#'@references
+#'Nathan, Aparna, et al. "Multimodally profiling memory T cells from a
+#'tuberculosis cohort identifies cell state associations with demographics,
+#'environment and disease." Nature immunology 22.6 (2021): 781-793.
+#'\doi{https://doi.org/10.1038/s41590-021-00933-1}
+#'
+#'@seealso
+#'Code repository: \url{https://github.com/immunogenomics/TB_Tcell_CITEseq}
+#'Genotype repository:
+#'\url{https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/study.cgi?study_id=phs002025.v1.p1}
+#'
+#'@export
+Nathan_tuberculosis <- function(){
+
+}
--- inst/scripts/make_metadata/Liu_COVID.R
+++ inst/scripts/make_metadata/Liu_COVID.R
@@ -7,4 +7,27 @@ library(tidyverse)
 patient_dat <- do.call(bind_rows,
                        lapply(liu, function(x) as_tibble(metadata(x))))
 
-patient_dat <-
+
+
+# This is the extra metadata file already downloaded
+
+timepoint_meta_f <- paste(c("https://ftp.ncbi.nlm.nih.gov/geo/series/",
+                            "GSE161nnn/GSE161918/suppl/GSE161918_allbatches.",
+                            "HTOandTimepointMetadata.csv.gz"), collapse = "")
+liu_timepoint <- "inst/metadata/Liu_timepoints.csv.gz"
+
+if (! file.exists(liu_timepoint)){
+    download.file(timepoint_meta_f, destfile = liu_timepoint)
+}
+
+liu_timepoint <- readr::read_delim(liu_timepoint) %>%
+    dplyr::mutate(across(Age, Status) = ~na_if(.x, "na"))
+
+# Metadata from Github repo - more patient information
+github_meta_f <- file.path("https://github.com/niaid/covid19-time-resolved/",
+                         "raw/main/A3/input.data/covid19.metadata.paper1.RData")
+
+github_tmp <- file.path(tempdir(), "liu_github.RData")
+
+download.file(github_meta_f, destfile = github_tmp)
+load(github_tmp)
