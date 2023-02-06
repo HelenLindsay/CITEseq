da0064034f1c428d1cf71a2b6b34dfaec6b9d0eb
helen <helen@hos67669.intranet.chuv>
2023-02-06 16:54:14 +0100

    starting documentation for Pont

---
+++ R/Pont_titration.R
@@ -0,0 +1,21 @@
+#' Obtain the Pont_titration data
+#'
+#' Obtain the PBMC CITE-seq titration data from Pont \emph{et al.} (2020).
+#'
+#'@details
+#'CITE-seq of PMBCs from a healthy donor. Cells were labelled with 12 antibodies
+#'in a 5-point titration curve.
+#'
+#'Data were downloaded from
+#'\url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE144434}
+#'
+#'A code repository is available (see below)
+#'@references
+#'\doi{10.1093/nargab/lqaa025}
+#'@seealso
+#'Code repository:
+#' \url{https://github.com/FredPont/single-cell-virtual-cytometer}
+#'@export
+Pont_ <- function(){
+
+}
