4fb7d4ef124dbe73472a5bc73ab3b6a2fa3bbb3d
Helen Lindsay <helen@hos67669.intranet.chuv>
2023-05-22 16:11:53 +0200

    documentation for Bai CAR T-cell data

---
+++ R/Bai_CART.R
@@ -0,0 +1,24 @@
+#' Obtain the Bai_CART data
+#'
+#' Obtain the CAR T-cell CITE-seq data from Bai \emph{et al.} (2022).
+#'
+#'@details
+#'CITE-seq of T-cells engineered to express chimeric antigen receptors (CAR)
+#'for CD19, from a healthy donor and two acute lymphoblastic leukemia patients.
+#'CAR T-cells were either unstimulated, stimulated with CD19, or stimulated
+#'with a non-antigen (mesothelin).
+#'
+#'Three samples were pooled per sequencing lane.
+#'
+#'Data were downloaded from
+#'\url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE197215}
+#'
+#'@references
+#'Bai, Zhiliang, et al. "Single-cell multiomics dissection of basal and
+#'antigen-specific activation states of CD19-targeted CAR T cells."
+#'Journal for Immunotherapy of Cancer 9.5 (2021).
+#'\doi{doi:10.1136/jitc-2020-002328}
+#'@export
+Bai_2022 <- function(){
+
+}
