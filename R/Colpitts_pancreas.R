239f4962e7f15078decf9aa91ce4d5180073fd6d
Helen Lindsay <helen@hos67669.intranet.chuv>
2023-05-23 15:55:54 +0200

    added Colpitts

---
+++ R/Colpitts_pancreas.R
@@ -0,0 +1,26 @@
+#' Obtain the Colpitts_pancreas data
+#'
+#' Obtain the pancreatic islet CITE-seq data from Colpitts \emph{et al.} (2023).
+#'
+#'@details
+#'CITE-seq of cells from enzymatically digested pancreatic islets, and
+#'undigested (mechanically separated) spleen cells from the same donor.
+#'Pancreatic islets enriched for CD45+ cells, and a subset was further sorted by flow
+#'cytometry to exclude cells expressing lineage markers, i.e. retain innate
+#'lymphoid cells (ILCs).
+#'
+#'CITE-seq data were downloaded from
+#'\url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE224767}
+#'
+#'Flow cytometry was also performed in this study to study antibody
+#'epitope susceptibility to enzymatic digestion and titrate antibody clones used
+#'in CITE-seq.
+#'
+#'@references
+#'Colpitts, Sarah J., et al. "Strategies for optimizing CITE-seq for human
+#'islets and other tissues." Frontiers in Immunology 14 (2023).
+#'\doi{10.3389/fimmu.2023.1107582}
+#'@export
+Colpitts_pancreas <- function(){
+
+}
